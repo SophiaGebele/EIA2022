c85cd9243ea0a66d04220eba91accb688c4c6aca
SophiaGebele <gebele.sophia@web.de>
2022-05-11 12:35:52 +0200

    new work

---
+++ Strand/StrandType.ts
@@ -0,0 +1,50 @@
+namespace strand{
+
+    window.addEventListener("load", start);
+
+    //Variablen
+    let crc2: CanvasRenderingContext2D = <CanvasRenderingContext2D>document.querySelector("canvas")?.getContext("2d");
+    
+
+    //start
+    function start(_event:Event):void{
+        let crc2: CanvasRenderingContext2D = <CanvasRenderingContext2D>document.querySelector("canvas")?.getContext("2d");
+       
+        //Wasser und Sand
+        let gardient=crc2.createLinearGradient(150,0,crc2.canvas.width,crc2.canvas.height);
+        gardient.addColorStop(0, "blue");   
+        gardient.addColorStop(1, "orange"); 
+        crc2.fillStyle=gardient;
+        crc2.fillRect(0,60,crc2.canvas.width,crc2.canvas.height);
+
+        //Mountain
+        crc2.moveTo(200, 60);
+        crc2.lineTo(220, 30);
+        crc2.lineTo(240, 60);
+        crc2.lineTo(270, 10);
+        crc2.lineTo(300, 60);
+        crc2.closePath();
+        crc2.fillStyle="black";
+        crc2.fill();
+        crc2.stroke();
+        
+        //Sun
+        crc2.beginPath();
+        crc2.arc(20, 10, 20, 0, 2*Math.PI);
+        crc2.stroke();
+        crc2.fillStyle="yellow";
+        crc2.fill();
+
+        //cloud 
+        crc2.beginPath();
+        crc2.arc(50, 50, 15, 0, 1*Math.PI);
+        crc2.arcTo(50, 35, 15, 0, 2*Math.PI);
+        crc2.stroke();
+
+        
+        
+
+    }//start Ende
+
+    
+}//NamespaceEnde
